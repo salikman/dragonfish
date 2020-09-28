a84ed9ebcf1c6415e759b1272aca1d1b9f212fc1
Max <savonikmaksim@gmail.com>
2020-09-28 14:22:20 +0300

    upd scroll

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@
 		e.preventDefault();
 		var hash = this.hash;
 		$('html, body').animate({
-			scrollTop: $(this.hash).offset().top
+			scrollTop: $(this.hash).offset().top - 85
 		}, 700);
 	});
     ///////////////////////////
